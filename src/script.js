3655497d8d9e44c8e6b690118209f81bd3c81cef
BillyPilgrim12 <jadwizycmarcin@gmail.com>
2019-10-29 22:46:13 +0100

    Add js to build & pack script in iife

--- src/script.js
+++ src/script.js
@@ -1,31 +1,33 @@
-var cta = document.querySelector(".cta");
-var testimonials = document.querySelector(".testimonials");
-var footer = document.querySelector(".footer");
-var form = {
-	body: document.querySelector(".form"),
-	inputContainer: document.querySelector(".form__inputContainer"),
-	input: document.querySelector(".form__input")
-};
+(function() {
+	var cta = document.querySelector(".cta");
+	var testimonials = document.querySelector(".testimonials");
+	var footer = document.querySelector(".footer");
+	var form = {
+		body: document.querySelector(".form"),
+		inputContainer: document.querySelector(".form__inputContainer"),
+		input: document.querySelector(".form__input")
+	};
 
-function ctaPositionHandler() {
-	var ctaHeight = cta.offsetHeight;
+	function ctaPositionHandler() {
+		var ctaHeight = cta.offsetHeight;
 
-	cta.style.bottom = "-" + ctaHeight / 2 + "px";
-	testimonials.style.paddingBottom = ctaHeight / 2 + 80 + "px";
-	footer.style.paddingTop = ctaHeight / 2 + 80 + "px";
-}
-
-function formSubmitHandler(event) {
-	event.preventDefault();
-	if (/\S+@\S+\.\S+/.test(form.input.value)) {
-		form.inputContainer.className = "form__inputContainer";
-		alert("Thank you for your email!");
-	} else {
-		form.inputContainer.className += " form__inputContainer--invalid";
+		cta.style.bottom = "-" + ctaHeight / 2 + "px";
+		testimonials.style.paddingBottom = ctaHeight / 2 + 80 + "px";
+		footer.style.paddingTop = ctaHeight / 2 + 80 + "px";
 	}
-}
 
-document.onload = ctaPositionHandler();
-document.onresize = ctaPositionHandler();
+	function formSubmitHandler(event) {
+		event.preventDefault();
+		if (/\S+@\S+\.\S+/.test(form.input.value)) {
+			form.inputContainer.className = "form__inputContainer";
+			alert("Thank you for your email!");
+		} else {
+			form.inputContainer.className += " form__inputContainer--invalid";
+		}
+	}
 
-form.body.addEventListener("submit", formSubmitHandler);
+	document.onload = ctaPositionHandler();
+	document.onresize = ctaPositionHandler();
+
+	form.body.addEventListener("submit", formSubmitHandler);
+})();
